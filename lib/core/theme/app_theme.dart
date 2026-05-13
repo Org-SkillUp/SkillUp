90f364235079ae448518c498a9b65829b7ea194e
Yuri-IQI <anthony.yuri.ff@gmail.com>
2026-05-12 22:58:54 -0300

    feat: adiciona app bar

--- lib/core/theme/app_theme.dart
+++ lib/core/theme/app_theme.dart
@@ -1,11 +1,28 @@
 import 'package:flutter/material.dart';
 
 class AppTheme {
-  static ThemeData lightTheme = ThemeData(
+
+  static ThemeData mainTheme = ThemeData(
+
     useMaterial3: true,
 
-    colorScheme: ColorScheme.fromSeed(seedColor: Colors.black),
+    colorScheme: ColorScheme.dark(
+      surface: const Color.fromARGB(255, 38, 54, 76),
+      primary: Colors.white,
+    ),
 
-    appBarTheme: const AppBarTheme(centerTitle: true),
+    scaffoldBackgroundColor: const Color.fromARGB(255, 33, 40, 48),
+
+    appBarTheme: const AppBarTheme(
+      backgroundColor: Color.fromARGB(255, 38, 54, 76),
+      surfaceTintColor: Colors.transparent,
+      foregroundColor: Colors.white,
+
+      elevation: 0,
+      scrolledUnderElevation: 2,
+
+      centerTitle: false,
+      leadingWidth: 240,
+    ),
   );
 }
